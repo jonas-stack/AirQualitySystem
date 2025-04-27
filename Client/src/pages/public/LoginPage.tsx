776ec1ae35a578d80fc9265be90c2ad48775e42f
based4real <45218699+based4real@users.noreply.github.com>
2025-04-27 19:35:26 +0200

    Design of login page

--- Client/src/pages/public/LoginPage.tsx
+++ Client/src/pages/public/LoginPage.tsx
@@ -1,33 +1,36 @@
 import { LoginForm } from "@/components/form/LoginForm";
+import Flex from "@/components/utils/Flex";
+import Grid from "@/components/utils/Grid";
 import { GalleryVerticalEnd } from "lucide-react";
 
 export default function LoginPage() {
 
     return (
-        <div className="grid min-h-svh lg:grid-cols-2">
-        <div className="flex flex-col gap-4 p-6 md:p-10">
-          <div className="flex justify-center gap-2 md:justify-start">
-            <a href="#" className="flex items-center gap-2 font-medium">
-              <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                <GalleryVerticalEnd className="size-4" />
-              </div>
-              Acme Inc.
-            </a>
-          </div>
-          <div className="flex flex-1 items-center justify-center">
-            <div className="w-full max-w-xs">
-              <LoginForm />
+        <Flex className="lg:h-screen lg:w-screen" gap="0" justifyContent="center" alignItems="center">
+            <Grid className="lg:grid-cols-2 border rounded" minHeight="sm:h-[100vh] lg:h-[60vh]" gap="0">
+                <Flex direction="column" className="p-6 md:p-10">
+                    <Flex justifyContent="center" className="md:justify-start">
+                    <a href="#" className="flex items-center gap-2 font-medium">
+                        <Flex className="h-6 w-6 rounded-md bg-primary text-primary-foreground" alignItems="center" justifyContent="center">
+                            <GalleryVerticalEnd className="size-4" />
+                        </Flex>
+                    Acme Inc.
+                    </a>
+                </Flex>
+                <Flex className="flex-1" alignItems="center" justifyContent="center">
+                    <div className="w-full max-w-xs">
+                        <LoginForm />
+                    </div>
+                </Flex>
+                </Flex>
+            <div className="relative hidden bg-muted lg:block">
+            <img
+                src="/placeholder.svg"
+                alt="Image"
+                className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
             </div>
-          </div>
-        </div>
-        <div className="relative hidden bg-muted lg:block">
-          <img
-            src="/placeholder.svg"
-            alt="Image"
-            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          />
-        </div>
-      </div>
-  
+        </Grid>
+      </Flex>
     )
 }
