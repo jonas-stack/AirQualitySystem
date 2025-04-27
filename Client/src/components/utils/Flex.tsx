d586fb56e453181fa7dcffe0914d914aec671e03
based4real <45218699+based4real@users.noreply.github.com>
2025-04-27 19:13:28 +0200

    Added clsx to combine classNames

--- Client/src/components/utils/Flex.tsx
+++ Client/src/components/utils/Flex.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import clsx from 'clsx';
 
 type FlexProps = {
   direction?: 'row' | 'column';
   gap?: string;
   justifyContent?: 'center' | 'start' | 'end' | 'between' | 'around' | 'evenly';
   alignItems?: 'center' | 'start' | 'end' | 'stretch';
+  className?: string;
   children: React.ReactNode;
 };
 
@@ -13,14 +15,19 @@ const Flex: React.FC<FlexProps> = ({
   gap = '4',
   justifyContent = 'start',
   alignItems = 'stretch',
+  className,
   children,
 }) => {
   return (
     <div
-      className={`flex ${direction === 'row' ? 'flex-row' : 'flex-col'} 
-                  gap-${gap} 
-                  justify-${justifyContent} 
-                  items-${alignItems}`}
+      className={clsx(
+        'flex',
+        direction === 'row' ? 'flex-row' : 'flex-col',
+        `gap-${gap}`,
+        `justify-${justifyContent}`,
+        `items-${alignItems}`,
+        className
+      )}
     >
       {children}
     </div>
--- Client/src/pages/IndexPage.tsx
+++ Client/src/pages/IndexPage.tsx
@@ -3,7 +3,7 @@ import Flex from "@/components/utils/Flex";
 export default function IndexPage() {
 
     return (
-        <Flex direction="column" justifyContent="center" alignItems="center">
+        <Flex className="w-screen h-screen" direction="column" justifyContent="center" alignItems="center">
             <h1>
                 Welcome!
             </h1>
