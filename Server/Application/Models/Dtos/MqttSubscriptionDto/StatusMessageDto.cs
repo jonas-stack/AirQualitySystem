be087d803b7d4490b04698b0b17d357bc5614d19
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2025-04-28 15:08:48 +0200

    Stop tracking appsettings.json and appsettings.Development.json

--- Server/Application/Models/Dtos/MqttSubscriptionDto/StatusMessageDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.Models.Dtos.MqttSubscriptionDto;
-
-public class StatusMessageDto
-{
-    public string Status { get; set; } // Matches "status"
-    public string DeviceId { get; set; } // Matches "device_id"
-    public long Timestamp { get; set; } // Matches "timestamp"
-}
