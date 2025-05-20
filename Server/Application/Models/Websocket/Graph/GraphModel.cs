04b3d1e70a78697f7fd60ba69432a0a96bea30fb
based4real <45218699+based4real@users.noreply.github.com>
2025-05-20 10:10:26 +0200

    forgot to change in the graphmodel

--- Server/Application/Models/Websocket/Graph/GraphModel.cs
+++ Server/Application/Models/Websocket/Graph/GraphModel.cs
@@ -4,6 +4,6 @@ public class GraphModel<T> : ApplicationBaseDto
 {
     public required string Identifier { get; set; }
     public required T Amount { get; set; }
-    public override string eventType { get; set; }
-    public override string topic { get; set; }
+    public override string EventType { get; set; }
+    public override string Topic { get; set; }
 }
