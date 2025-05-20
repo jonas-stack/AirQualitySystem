a6f582a4cf848a187cc57a8d670c37b22914931b
based4real <45218699+based4real@users.noreply.github.com>
2025-05-20 10:09:57 +0200

    forgot one conflict

--- Server/Application/Models/Dtos/ApplicationBaseDto.cs
+++ Server/Application/Models/Dtos/ApplicationBaseDto.cs
@@ -2,11 +2,6 @@
 
 public abstract class ApplicationBaseDto
 {
-<<<<<<< HEAD
-    public abstract string eventType { get; set; }
-    public abstract string topic { get; set; }
-=======
     public abstract string EventType { get; set; }
     public abstract string Topic { get; set; }
->>>>>>> Development
 }
